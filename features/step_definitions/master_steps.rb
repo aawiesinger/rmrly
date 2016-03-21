ad3e5d64ae11fbd0ad3cb3676145c7c38e4f82db
tommyk154 <tkargul1@binghamton.edu>
2016-03-21 15:28:20 +0000

    added steps

--- features/step_definitions/master_steps.rb
+++ features/step_definitions/master_steps.rb
@@ -21,3 +21,12 @@ end
 When /^(?:|I )fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
   fill_in(field, :with => value)
 end
+
+Then /^(?:|I )should be on (.+)$/ do |page_name|
+  current_path = URI.parse(current_url).path
+  if current_path.respond_to? :should
+    current_path.should == path_to(page_name)
+  else
+    assert_equal path_to(page_name), current_path
+  end
+end
