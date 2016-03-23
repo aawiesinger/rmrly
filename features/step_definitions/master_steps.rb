16f2bd8381a50d499bd7b70693ae928825d12ebf
Jack Fischer <jfische8@binghamton.edu>
2016-03-23 15:38:27 +0000

    test working

--- features/step_definitions/master_steps.rb
+++ features/step_definitions/master_steps.rb
@@ -1,5 +1,9 @@
-Given /^(?:|I )am on (.+)$/ do |page_name|
-  visit path_to(page_name)
+#Given /^(?:|I )am on (.+)$/ do |page_name|
+#  visit path_to(page_name)
+#end
+
+Given /^I am on "([^"]*)"$/ do |path|
+  visit path
 end
 
 Given /^(?:|I )am a user with email "([^"]*)" and password "([^"]*)"$/ do |email, password|
@@ -29,4 +33,16 @@ Then /^(?:|I )should be on (.+)$/ do |page_name|
   else
     assert_equal path_to(page_name), current_path
   end
+end
+
+#Then /^Ishould see "([^"]*)" in the selector "([^"]*)"$/ do |text, selector|
+#  page.should have_selector selector, content: text
+#end
+# 
+#Then /^I should see "([^"]*)" in a link$/ do |text|
+#  page.should have_link text
+#end
+
+Then /^I should see "([^"]*)"$/ do |text|
+  page.should have_content text
 end
