9be2564c06e3be6b40260975d5d8b0faacb38ad6
tommyk154 <tkargul1@binghamton.edu>
2016-03-23 19:17:33 +0000

    database messed up

--- db/migrate/20160323190041_create_users.rb
+++ db/migrate/20160323190041_create_users.rb
@@ -1,10 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
-    create_table :users do |t|
-      t.string :name
-      t.string :email
+    # create_table :users do |t|
+    #   t.string :name
+    #   t.string :email
 
-      t.timestamps null: false
-    end
+    #   t.timestamps null: false
+    # end
+    drop_table :users
   end
 end
