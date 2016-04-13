8bdd7c2c1e1deee3630105d7d10019d416b86c45
Josh <jlevin18@binghamton.edu>
2016-04-13 21:22:02 +0000

    passing all rspec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,5 @@ class UsersController < ApplicationController
       @logged_in_user = User.find(params[:logged_in_user])
       @matches = Array.new
       @logged_in_user.matches.each {|user| @matches.push(User.find_by_username(user))}
-      return @matches
    end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 require 'users_controller'
 
 describe UsersController, type: :controller do
+    describe '#dashboard' do
+       it 'should render the :dashboard template' do
+          id = 8
+          FactoryGirl.create(:user, username: "fake_user", id: id, matches: [])
+          get :dashboard, logged_in_user: id
+          expect(response).to render_template("dashboard")
+       end
+    end
 end
