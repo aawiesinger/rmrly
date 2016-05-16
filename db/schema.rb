bf24f34163b36863af654acc537f9b17bb31a3d6
Annika Wiesinger <aawiesinger@gmail.com>
2016-05-16 23:58:29 +0000

    css and stuff

--- db/schema.rb
+++ db/schema.rb
@@ -24,11 +24,11 @@ ActiveRecord::Schema.define(version: 20160502203554) do
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "password"
+    t.string   "name"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.string   "name"
     t.string   "matches"
   end
 
