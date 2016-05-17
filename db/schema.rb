16c59cbf5e80a4ec99545aa9c8714ee176d3485a
Annika Wiesinger <aawiesinger@gmail.com>
2016-05-17 16:02:47 +0000

    updates

--- db/schema.rb
+++ db/schema.rb
@@ -25,8 +25,8 @@ ActiveRecord::Schema.define(version: 20160512004556) do
     t.string   "email"
     t.string   "password"
     t.string   "name"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
     t.string   "matches"
