27c01b2e0a0038c338a75208ee175fe97b00fb97
Josh <jlevin18@binghamton.edu>
2016-05-18 02:41:32 +0000

    added rspec for users controller view matches

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -142,13 +142,41 @@ RSpec.describe UsersController, type: :controller do
   end
   
   describe '#view_matches' do
+    before :each do
+      @id1 = 33
+      @id2 = 44
+      @id3 = 55
+      @id4 = 66
+      @name1 = "abc1"
+      @name2 = "abc2"
+      @name3 = "abc3"
+      @name4 = "abc4"
+      @email1 = 'abc1@gmail.com'
+      @email2 = 'abc2@gmail.com'
+      @email3 = 'abc3@gmail.com'
+      @email4 = 'abc4@gmail.com'
+      @user1 = FactoryGirl.create(:user, name: @name1, id: @id1, matches: [], email: @email1)
+      @user2 = FactoryGirl.create(:user, name: @name2, id: @id2, matches: [@name3, @name4], email: @email2)
+      @user3 = FactoryGirl.create(:user, name: @name3, id: @id3, matches: [@name2], email: @email3)
+      @user4 = FactoryGirl.create(:user, name: @name4, id: @id4, matches: [@name2], email: @email4)
+    end
        it 'should render the :view_matches template' do
-          id = 8
-          FactoryGirl.create(:user, name: "fake_user", id: id, matches: [])
-          get :view_matches, logged_in_user: id
+          get :view_matches, logged_in_user: @id1
           expect(response).to render_template("view_matches")
        end
+       describe "if the user already has matches" do
+        it "should find all the correct matches for this user" do
+            get :view_matches, logged_in_user: @id2
+            expect(assigns(:matches)).to contain_exactly(@user3, @user4)
+          end
+        end
+        describe "if the user doesn't already have matches" do
+          it "should not find any matches" do
+            get :view_matches, logged_in_user: @id1
+            expect(assigns(:matches)).to be_empty
+        end
      end
+   end
 
   describe "DELETE #destroy" do
     it "destroys the requested user" do
